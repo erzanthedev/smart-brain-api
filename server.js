e77ae6616cc2af58f40862bb2ab01d300531838f
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-08 20:30:34 +1000

    adding response

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json("success");
+  res.json("successful it is working");
 });
 
 app.post("/signin", (req, res) => {
