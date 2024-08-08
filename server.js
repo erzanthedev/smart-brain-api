a687954ea4b8032153d9d0111da8e6d49024bef8
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-08 21:05:37 +1000

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image.js");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-fitted-82588",
     port: 5432,
     user: "michaelerzanandoh",
     password: "",
